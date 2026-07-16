e74f163b504a5011f86ff1d0c7bf84a569e9fbb2
Joshua Pchumba <joshuapchumba64@gmail.com>
2026-07-16 04:48:43 +0300

    feat: add memory module barrel export

---
+++ src/memory/index.mjs
@@ -0,0 +1,4 @@
+export { loadConversation, appendToConversation, clearConversation } from "./conversationMemory.mjs";
+export { initDatabase, getDatabase } from "./database.mjs";
+export { searchConversations } from "./search.mjs";
+export { saveProjectMemory, loadProjectMemory } from "./memoryBank.mjs";
